cdb645a2b3bdbc16b61bbbb9ffd947d5f1bd7264
Dmitry Ivanov <ivadmi5@gmail.com>
2017-09-03 17:38:02 +0300

    rename 'usage' to 'hits'

--- pg_linegazer--1.0.sql
+++ pg_linegazer--1.0.sql
@@ -4,6 +4,6 @@ AS 'pg_linegazer', 'linegazer_clear_pl'
 LANGUAGE C STRICT;
 
 CREATE OR REPLACE FUNCTION linegazer_simple_report(procid REGPROCEDURE)
-RETURNS TABLE (line INT4, usage INT4, code TEXT)
+RETURNS TABLE (line INT4, hits INT4, code TEXT)
 AS 'pg_linegazer', 'linegazer_simple_report_pl'
 LANGUAGE C STRICT;
